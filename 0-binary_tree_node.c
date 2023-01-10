5123880a86c6f0e2d8ab8dd160302bdc92dd41d5
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-01-10 12:49:53 -0400

    fixed betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -22,4 +22,3 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
     return (new_node);
 }
-
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,9 +5,9 @@
  * @parent: is a pointer to the node to insert the left-child in
  * @value: is the value to store in the new node
  * Return: a pointer to the created node, or NULL on failure or if parent is NULL
-*/
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
- {
+{
     binary_tree_t *new_node;
 
     if (parent == NULL)
