75cc9f4c7b8531b4fd922b45d439105ec6e29717
MelissaAT <melissaarroyotorres9806@gmail.com>
2023-01-12 21:02:53 +0000

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-size_t higher(size_t right_height, size_t left_height);
+size_t higher(size_t right, size_t left);
 /**
  * binary_tree_height - function that ,easures the height of a binary tree
  * @tree: is a pointer to the root node of the tree to measure the height
@@ -7,34 +7,34 @@ size_t higher(size_t right_height, size_t left_height);
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height, high;
+	size_t left, right, high;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 	if (tree->left)
-		left_height = binary_tree_height(tree->left) + 1;
+		left = binary_tree_height(tree->left) + 1;
 
 	if (tree->right)
-		right_height = binary_tree_height(tree->right) + 1;
+		right = binary_tree_height(tree->right) + 1;
 
-	high = higher(right_height, left_height);
+	high = higher(right, left);
 		return (high);
 }
 
 /**
  * higher - find the highest subtree
- * @rigth_height: right subtree
+ * @right_height: right subtree
  * @left_height: left subtree
  * Return: the highest subtree
  */
-size_t higher(size_t right_height, size_t left_height)
+size_t higher(size_t right, size_t left)
 {
-	if (right_height > left_height)
+	if (right > left)
 	{
-		return (right_height);
+		return (right);
 	}
 	else
-		return (left_height);
+		return (left);
 }
