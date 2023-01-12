ce89cb0fff24e3bd34db794a178ff0f17a1c1060
MelissaAT <melissaarroyotorres9806@gmail.com>
2023-01-12 17:38:13 +0000

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,10 +13,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree->left_height)
+	if (tree->left)
 		left_height = binary_tree_height(tree->left) + 1;
 
-	if (tree->right_height)
+	if (tree->right)
 		right_height = binary_tree_height(tree->right) + 1;
 
 	high = higher(right_height, left_height);
