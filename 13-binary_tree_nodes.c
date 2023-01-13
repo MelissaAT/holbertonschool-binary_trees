6e18b475de40520a5f1e7c67e6964db48eda7c01
MelissaAT <melissaarroyotorres9806@gmail.com>
2023-01-13 17:38:59 +0000

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left ! = NULL || tree->right != NULL)
+	if (tree->left != NULL || tree->right != NULL)
 	{
 		return (binary_tree_nodes(tree->left) +
 				binary_tree_nodes(tree->right));
