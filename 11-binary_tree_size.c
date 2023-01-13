91a0178ddfd603d35ce916454ffefa130a25b5c4
MelissaAT <melissaarroyotorres9806@gmail.com>
2023-01-13 17:21:00 +0000

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - function that measures the size 
+ * binary_tree_size - function that measures the size
  * of a binary tree
  * @tree: is a pointer to the root node of the tree to
  * measures the size
