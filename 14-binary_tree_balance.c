1b3ca0011af535d15a9c6f8095d9c5387df861ed
MelissaAT <melissaarroyotorres9806@gmail.com>
2023-01-13 17:57:54 +0000

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,7 +15,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
-	left = binary_tree_height(tree->left);
+	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
